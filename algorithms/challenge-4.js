76df6053887f1901ff45a67cfebb821cd1cc412b
Sunkanmi19 <100348387+Sunkanmi19@users.noreply.github.com>
2022-02-26 14:03:04 +0100

    after the whole work this what i get

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,19 @@
-function highestSum(x, y) {}
+function highestSum(x, y) {
+  function sum(a) {
+    let length = a.length;
+    let summation = 0;
+    for (let i = 0; i < length; i++) {
+      summation += a[i];
+    {
+      return summation;
+    }
+    let firstsum = sum(x);
+    let secondsum = sum(y);
 
+    if (firstsum > secondsum) {
+      return firstsum;
+    } else {
+      return secondsum;
+    }
+  }
 module.exports = highestSum
